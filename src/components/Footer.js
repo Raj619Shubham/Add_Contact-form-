ab94a10553bbd4325944695393e4308caa32a97e
Raj Shubham <sraj17297@gmail.com>
2021-07-18 19:22:31 +0530

    final commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <script src="index.js"></script>
+      <footer class="copyright">
+        <div className="footer">
+          Copyright © 2021 .All rights reserved by{" "}
+          <a href="https://www.instagram.com/ra___shu/">
+            <em>Raj Shubham❤️</em>
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
